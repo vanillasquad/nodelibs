28300638c9c340306d6ad374390886656372e043
Andrew MacMurray <a.macmurray@icloud.com>
2016-02-16 22:50:31 +0000

    uncomment madlibs.js

--- server/madlibs.js
+++ server/madlibs.js
@@ -1,35 +1,34 @@
 module.exports = {
     "get": [
-        // {
-        //     "sentences": [ "'Golly!' she said as she ", 0, " his head. He ", 1, " her. 'What are you doing?' he exclaimed ", 2, ".  This was turning into a ", 3, " evening." ],
-        //     "required": ["verbs","verbs","adverbs", "adjectives"],
-        //     "hints": ["Verb (Past tense)", "Verb (Past tense)", "Adverb", "Adjective" ]
-        // },
-        // {
-        //     'sentences': ['Hi, my name is Jack, and I love ', 0, '. ', 1, ' is one of my favourite things to do. I also like to ', 2, ' with my pet beaver. If people were to describe me, they would say I am ', 3, "."],
-        //     'required': ['nouns', 'verbs', 'verbs', 'adjectives'],
-        //     'hints': ['Noun (Plural)', 'Verb (Present participle -ing)', 'Verb (Present tense)', 'Adjective']
-        // },
-        // {
-        //     "sentences": [ "He touched the ", 0, " and ", 1, " the ", 2, ". 'Wow!' he said, this is ", 3, "."],
-        //     "required": ["nouns","verbs","nouns", "adjectives"],
-        //     "hints": ["Noun", "Verb (Past tense)", "Noun", "Adjective"]
-        // },
-        // {
-        //     "sentences": ["Andrew was ", 0, " ", 1, " in the park. He saw a ", 2, " ", 3, " and he said, 'Whoa, that's the ", 4, " I ever saw!"],
-        //     "required": ["verbs", "adverbs", "adjectives", "nouns", "adjectives"],
-        //     "hints": ["Verb (Present participle -ing)", "Adverb", "Adjective", "Noun (singular)", "Adjective (superlative -est)"]
-        // },
-        // {
-        //     "sentences": ["Elias was ", 0, " at his house. He was ", 1, " about this, because he really wanted to be ", 2, " instead. He said to his pet ", 3, ", while stroking it ", 4, ", 'Perhaps next weekend!'"],
-        //     "required": ["verbs", "adjectives", "verbs", "nouns", "adverbs"],
-        //     "hints": ["Verb (Present participle -ing)", "Adjective (feeling)", "Verb (Present participle -ing)", "Noun (animal)", "Adverb"]
-        // },
+        {
+            "sentences": [ "'Golly!' she said as she ", 0, " his head. He ", 1, " her. 'What are you doing?' he exclaimed ", 2, ".  This was turning into a ", 3, " evening." ],
+            "required": ["verbs","verbs","adverbs", "adjectives"],
+            "hints": ["Verb (Past tense)", "Verb (Past tense)", "Adverb", "Adjective" ]
+        },
+        {
+            'sentences': ['Hi, my name is Jack, and I love ', 0, '. ', 1, ' is one of my favourite things to do. I also like to ', 2, ' with my pet beaver. If people were to describe me, they would say I am ', 3, "."],
+            'required': ['nouns', 'verbs', 'verbs', 'adjectives'],
+            'hints': ['Noun (Plural)', 'Verb (Present participle -ing)', 'Verb (Present tense)', 'Adjective']
+        },
+        {
+            "sentences": [ "He touched the ", 0, " and ", 1, " the ", 2, ". 'Wow!' he said, this is ", 3, "."],
+            "required": ["nouns","verbs","nouns", "adjectives"],
+            "hints": ["Noun", "Verb (Past tense)", "Noun", "Adjective"]
+        },
+        {
+            "sentences": ["Andrew was ", 0, " ", 1, " in the park. He saw a ", 2, " ", 3, " and he said, 'Whoa, that's the ", 4, " I ever saw!"],
+            "required": ["verbs", "adverbs", "adjectives", "nouns", "adjectives"],
+            "hints": ["Verb (Present participle -ing)", "Adverb", "Adjective", "Noun (singular)", "Adjective (superlative -est)"]
+        },
+        {
+            "sentences": ["Elias was ", 0, " at his house. He was ", 1, " about this, because he really wanted to be ", 2, " instead. He said to his pet ", 3, ", while stroking it ", 4, ", 'Perhaps next weekend!'"],
+            "required": ["verbs", "adjectives", "verbs", "nouns", "adverbs"],
+            "hints": ["Verb (Present participle -ing)", "Adjective (feeling)", "Verb (Present participle -ing)", "Noun (animal)", "Adverb"]
+        },
 		{
             "sentences": ["Katherine was ", 0, " working on her ", 1, " when suddenly the window blew open and a ", 2, " ", 3, " burst into the room! Enraged she turned round and exclaimed '", 4, "! I know where you keep your ", 5, "!'"],
             "required": ["adverbs", "nouns", "adjectives", "nouns", "nouns", "nouns"],
             "hints": ["Adverb", "Noun", "Adjective", "Noun", "Noun (plural)", "Noun (plural, part of the body)"]
         }
-		// "One day, Katherine was furiously working on her ", ", when suddenly the window blew open and a ", " ", "burst into the room!", "Enraged she turned round and screamed: ", "!"
     ]
 };
