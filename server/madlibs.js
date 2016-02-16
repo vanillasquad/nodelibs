93cbcd582b7cd29532bc1997ae7e31b62d838613
Katherine Bowler <katherine.bowler@gmail.com>
2016-02-16 15:16:42 +0000

    Add madlib about Jack

--- server/madlibs.js
+++ server/madlibs.js
@@ -3,7 +3,12 @@ module.exports = {
         "sentences": [ "'Golly!' she said as she ", 0, " his head. He ", 1, " her. 'What are you doing?' he exclaimed ", 2, ".  This was turning into a ", 3, " evening" ],
         "required": ["verbs","verbs","adverbs", "adjectives"],
         "hints": ["Verb (Past tense)", "Verb (Past tense)", "Adverb", "Adjective" ]
-    }]
+        },
+        {
+        'sentences': ['Hi, my name is Jack, and I love ', 0, '.My favourite thing to do is ', 1, '. I like to ', 2, 'with my pet beaver. If people were to describe me, they would say I am', 3],
+        'required': ['nouns', 'verbs', 'verbs', 'adjectives'],
+        'hints': ['Noun (Plural)', 'Verb (Present tense)', 'Verb (Present tense)', 'Adjective']
+        }]
 };
 //
 // {
