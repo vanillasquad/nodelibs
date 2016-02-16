acf32fe6acc44104b26c2224ad18b611cfb1a417
Katherine Bowler <katherine.bowler@gmail.com>
2016-02-16 15:53:16 +0000

    Add madlib about Andrew

--- server/madlibs.js
+++ server/madlibs.js
@@ -1,18 +1,29 @@
 module.exports = {
-    "get": [ {
-        "sentences": [ "'Golly!' she said as she ", 0, " his head. He ", 1, " her. 'What are you doing?' he exclaimed ", 2, ".  This was turning into a ", 3, " evening" ],
-        "required": ["verbs","verbs","adverbs", "adjectives"],
-        "hints": ["Verb (Past tense)", "Verb (Past tense)", "Adverb", "Adjective" ]
-        },
+    "get": [
+        // {
+        //     "sentences": [ "'Golly!' she said as she ", 0, " his head. He ", 1, " her. 'What are you doing?' he exclaimed ", 2, ".  This was turning into a ", 3, " evening." ],
+        //     "required": ["verbs","verbs","adverbs", "adjectives"],
+        //     "hints": ["Verb (Past tense)", "Verb (Past tense)", "Adverb", "Adjective" ]
+        // },
+        // {
+        //     'sentences': ['Hi, my name is Jack, and I love ', 0, '.', 1, 'is one of my favourite things to do. I also like to ', 2, ' with my pet beaver. If people were to describe me, they would say I am ', 3, "."],
+        //     'required': ['nouns', 'verbs', 'verbs', 'adjectives'],
+        //     'hints': ['Noun (Plural)', 'Verb (Present participle -ing)', 'Verb (Present tense)', 'Adjective']
+        // },
+        // {
+        //     "sentences": [ "He touched the ", 0, " and ", 1, " the ", 2, ". 'Wow!' he said, this is ", 3, "."],
+        //     "required": ["noun","verb","noun", "adjective"],
+        //     "hints": ["Noun", "Verb (Past tense)", "Noun", "Adjective"]
+        // },
         {
-        'sentences': ['Hi, my name is Jack, and I love ', 0, '.My favourite thing to do is ', 1, '. I like to ', 2, 'with my pet beaver. If people were to describe me, they would say I am', 3],
-        'required': ['nouns', 'verbs', 'verbs', 'adjectives'],
-        'hints': ['Noun (Plural)', 'Verb (Present tense)', 'Verb (Present tense)', 'Adjective']
-        }]
+            "sentences": ["Andrew was ", 0, " ", 1, " in the park. He saw a ", 2, " ", 3, " and he said, 'Whoa, that's the ", 4, " I ever saw!"],
+            "required": ["verbs", "adverbs", "adjectives", "nouns", "adjectives"],
+            "hints": ["Verb (Present participle -ing)", "Adverb", "Adjective", "Noun (singular)", "Adjective (superlative)"],
+        },
+        // {
+        //     "sentences": [],
+        //     "required": [],
+        //     "hints": [],
+        // }
+    ]
 };
-//
-// {
-//     "sentences": [ "He touched the ", 0, " and ", 1, " the ", 2, ". 'Wow!' he said, this is ", 3 ],
-//     "required": ["noun","verb","noun", "adjective"],
-//     "hints": ["Noun", "Verb (Past tense)", "Noun", "Adjective"]
-// }
